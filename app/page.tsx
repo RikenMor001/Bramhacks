e8b509e888b7117f794f13f9b2b61c9a7136c9e5
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-10-31 18:19:04 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import React from 'react'
+import Signup from './components/Signup'
 
 export default function Home(){
-  const router = useRouter();
-  return <div className='flex justify-center items-center h-screen bg-slate-100 text-lg font-bold'>
-    <button onClick={() => router.push('/dashboard')}>
-      Go to dashboard
-    </button>
+  return <div>
+    <Signup />
   </div>
 }
+
 
 
 
