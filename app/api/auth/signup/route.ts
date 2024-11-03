7c8a3d624e19b8a9f594bec148f140428f8b5c10
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-11-03 12:48:54 -0500

    Update route.ts

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,69 +1,97 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { NextResponse } from "next/server";
+import { ChangeEvent, useState } from "react";
 
-// Take the req.body inputs and check if the user already exists or not 
-// Than get the headers and check the headers are authorized are not 
-// Than split the token from the header after requesting it back
-// Than check if the token passed is correct or not 
-// Signup sends the token to the user and while signing in the user will send back the token 
-// Do the password validation in sing in and not sign up 
-// Compare the password (Use bcrypt for that) 
-// Than create the user in prims.user.create({data: {email: body.email, password: body.password}})
+export default function Signup() {
+    const [ username, setUsername ] = useState<string>('');
+    const [ password, setPassword ] = useState<string>('');
+    const [ email, setEmail ] = useState<string>('');
+    const [error, setError] = useState<string | any>(null);
+    const router = useRouter();
+
+    const handleClick = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/auth/signup", { username,email, password });
+            if (response.status === 200) {
+                router.push("/signin"); 
+            } else {
+                setError("Invalid credentials. Please try again."); 
+            }
+        } catch (error: any) {
+            if (error.response?.status === 404) {
+                setError("User does not exist. Please sign up."); 
+            } else {
+                setError(error.response?.data?.message || "Sign-in failed. Please try again."); 
+            }
+        }
+    };
+
+    return (
+        <div className="flex justify-center bg-zinc-300	items-center h-screen flex-col">
+            <div className="text-center mb-8">
+                <h1 className="font-bold text-4xl text-gray-800 mb-2">Create Your Account</h1>
+                <p className="text-gray-600">Join us and drive towards a sustainable future.</p>
+            </div>
+
+            <Input
+                label="Username"
+                type="text"
+                placeholder="Enter your username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            
+            <Input
+                label="Email"
+                type="email"
+                placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
+            /> 
+
+            <Input
+                label="Password"
+                type="password"
+                placeholder="Enter your password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
 
-import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
+            <div className="mt-6">
+                <button
+                    onClick={handleClick}
+                    className="bg-gray-900 text-white py-3 px-8 rounded-lg hover:bg-gray-800 transition duration-200">
+                    Sign Up
+                </button>
+            </div>
 
-const prisma = new PrismaClient();
-export async function POST(req: NextRequest) {
-  try {
-    const { username, email, password } = await req.json();
-
-    const existingUser = await prisma.user.findUnique({
-      where: { 
-        email: email,
-        username: username 
-      },
-    });
-
-    if (existingUser) {
-      return NextResponse.json(
-        { message: "User already exists" },
-        { status: 400 }
-      );
-    }
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: passwordHash,
-      },
-    });
-
-    // Generate JWT token upon  signup
-    const JWT_SECRET = process.env.JWT_SECRET as string;
-    const userToken = jwt.sign({ id: newUser.id }, JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    return NextResponse.json(
-      { message: "User created", token: userToken },
-      { status: 200 }
+            <div className="mt-4 flex items-center text-gray-600">
+                <p className="mr-2">Already have an account?</p>
+                    <button onClick={() => {
+                        router.push("/signin")
+                    }} className="text-slate-600 hover:underline">Sign In</button>
+            </div>
+        </div>
     );
-  } catch (error) {
-    console.error("Signup error:", error);
-    return NextResponse.json(
-      { error: "An error occurred during signup" },
-      { status: 500 }
-    );
-  }
 }
 
-export function GET() {
-  return NextResponse.json({
-    message: "Welcome to localhost:3000/signup"
-  });
+interface inputTypes {
+    label: string;
+    placeholder: string;
+    type?: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Input({ label, placeholder, type, onChange }: inputTypes) {
+    return (
+        <div className="flex flex-col items-center w-full mb-5">
+            <label className="font-semibold text-lg text-gray-800 mb-2">{ label }</label>
+            <input
+                placeholder={ placeholder }
+                type={ type }
+                onChange={ onChange }
+                className="w-1/5 bg-gray-200 text-gray-900 text-sm rounded-lg shadow-md p-4 border border-gray-400"
+            />
+        </div>
+    );
 }
